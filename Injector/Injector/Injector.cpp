fa231135cc480642c96a0c644cde5cfcd9dcea26
jjoonn1 <jjonathanetc@gmail.com>
2020-05-16 18:18:12 +0300

    Now there is a problem with getErrorMessage.

--- Injector/Injector/Injector.cpp
+++ Injector/Injector/Injector.cpp
@@ -3,10 +3,8 @@
 #include <memory>
 #include "Injector.h"
 
-using namespace std;
-
 int main() {
-	unique_ptr<string> dllPath = make_unique<string>("C:\\code\\MessageBoxDLL\\MessageBox\\x64\\Release\\MessageBox.dll");
+	string dllPath = "C:\\code\\MessageBoxDLL\\MessageBox\\x64\\Release\\MessageBox.dll";
 	cout << "Enter pid: ";
 	int pid;
 	cin >> pid;
@@ -14,29 +12,35 @@ int main() {
 	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 	printMessage(processHandle, string("OpenProcess"));
 
-	HANDLE pDllPath = VirtualAllocEx(processHandle, NULL, strlen((*dllPath.get()).c_str()) + 1, MEM_COMMIT, PAGE_READWRITE);
+	HANDLE pDllPath = VirtualAllocEx(processHandle, NULL, strlen(dllPath.c_str()) + 1, MEM_COMMIT, PAGE_READWRITE);
 	printMessage(pDllPath, string("VirtualAllocEx"));
 
-	BOOL result = WriteProcessMemory(processHandle, pDllPath, (LPVOID)(dllPath.get()), strlen((*dllPath.get()).c_str()) + 1, 0);
+	BOOL result = WriteProcessMemory(processHandle, pDllPath, (LPVOID)(dllPath.c_str()), strlen(dllPath.c_str()) + 1, 0);
 	printMessage(HANDLE(result), string("WriteProcessMemory"));
 
 	HANDLE remoteThread = CreateRemoteThread(processHandle, 0, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("Kernel32.dll"), "LoadLibraryA"), pDllPath, 0, NULL);
 	printMessage(remoteThread, string("CreateRemoteThread"));
 
 	WaitForSingleObject(remoteThread, INFINITE);
+
+	DWORD exitCode = 0;
+	result = GetExitCodeThread(remoteThread, &exitCode);
+	printMessage((HANDLE)result, string("GetExitCodeThread"));
+	cout << "exit code of thread is: " << exitCode;
 }
 
 string getErrorMessage(int errorNumber) {
-	LPSTR errorMessage = nullptr;
+	LPSTR* messageRaw = nullptr;
 	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, errorNumber, NULL, (LPSTR)&errorMessage, 0, NULL);
-	return string(errorMessage);
+		NULL, errorNumber, NULL, (LPSTR)messageRaw, 0, NULL);
+	unique_ptr<char, decltype(&LocalFree)> errorMessage(*messageRaw, &LocalFree);
+	return string(errorMessage.get());	
 }
 
 void printMessage(HANDLE result,string &functionName) {
 	if (!result) {
 		auto lastError = GetLastError();
-		string errorMessage = string(getErrorMessage(lastError));
+		string errorMessage = getErrorMessage(lastError);
 		cout << "An error occured in " << functionName << ", error number: " << lastError << ", " << errorMessage;
 	}
 }
